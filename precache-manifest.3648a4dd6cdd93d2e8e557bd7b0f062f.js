aac2559b86b507f569c988c5b411fc9fac3f4ec2
justineearlfern <fernandez1302885@ceu.edu.ph>
2020-10-08 19:02:07 +0800

    Updates

---
+++ precache-manifest.3648a4dd6cdd93d2e8e557bd7b0f062f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0858c88bf9e07d3d9db841a759f21978",
+    "url": "/webApp00/index.html"
+  },
+  {
+    "revision": "dae714693904f31cc941",
+    "url": "/webApp00/static/css/main.01c5b57f.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/webApp00/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webApp00/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae714693904f31cc941",
+    "url": "/webApp00/static/js/main.eb3d2642.chunk.js"
+  },
+  {
+    "revision": "d383cadff6462e7b837f",
+    "url": "/webApp00/static/js/runtime-main.af4b4e29.js"
+  }
+]);
